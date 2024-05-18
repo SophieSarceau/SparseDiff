d32808f17c00e2598440b6b35e166cfc64081ac3
SophieSarceau <zxz.kryptonian@gmail.com>
2024-05-18 14:38:46 +0800

    protein

--- sparse_diffusion/sparse.sh
+++ sparse_diffusion/sparse.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+#SBATCH --job-name=scxlab0064_job
+#SBATCH --qos gpugpu
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
 #SBATCH -p vip_gpu_ailab
-#SBATCH --gpus=4
 #SBATCH -A ai4agr
+##SBATCH -w SH-IDC1-10-140-24-123
 #SBATCH -o /home/bingxing2/ailab/scxlab0064/Document/PPI/SparseDiff/outputs/slurm-out/protein.out
 
-python main.py
+export NCCL_ALGO=Ring
+export NCCL_MAX_NCHANNELS=16
+export NCCL_MIN_NCHANNELS=16
+export NCCL_DEBUG=INFO
+export NCCL_TOPO_FILE=/home/bingxing2/apps/nccl/conf/dump.xml
+export NCCL_IB_HCA=mx5_0,mlx5_2
+export NCCL_IB_GID_INDEX=3
+export NCCL_IB_TIMEOUT=23
+export NCCL_IB_RETRY_CNT=7
+
+srun python main.py
---
+++ sparse_diffusion/test.py
@@ -0,0 +1,7 @@
+import os
+print("import os finished")
+from rdkit import Chem
+print("import Chem finished")
+from rdkit.Chem import Draw, AllChem
+from rdkit.Geometry import Point3D
+from rdkit import RDLogger
